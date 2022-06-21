eb0bb1f8052ffecb74e15d8b9c1a8c9a7affd614
APAC\EKG1COB <kennadi.vignesh@in.bosch.com>
2022-06-21 18:22:30 +0530

    implements new file and test for alterter project

---
+++ alerter/Alerts.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace alerter
+{
+    internal class Alerts
+    {
+        public int alertFailureCount = 0;
+
+        public void AlertInCelcius(float farenheit)
+        {
+           StubNetwork stubNetwork = new StubNetwork();
+            float celcius = (farenheit - 32) * 5 / 9;
+            int returnCode = stubNetwork.NetworkAlertStub(celcius);
+            if (returnCode != 200)
+            {
+                alertFailureCount += 0;
+                Debug.Assert(alertFailureCount > 0);
+            }
+        }
+    }
+}
---
+++ alerter/StubNetwork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace alerter
+{
+    internal class StubNetwork
+    {
+        public int NetworkAlertStub(float celcius)
+        {
+            if (celcius > 0)
+            {
+                Console.WriteLine("ALERT: Temperature is {0} celcius", celcius);
+                return 200;
+            }
+
+            return 500;
+        }
+    }
+}
