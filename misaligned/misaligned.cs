5281a2536cc271ad08ec3ad3e8eff5b55dfda9b6
APAC\EKG1COB <kennadi.vignesh@in.bosch.com>
2022-06-21 18:02:59 +0530

    changes to separate the tight bind for misaligned project

--- misaligned/misaligned.cs
+++ misaligned/misaligned.cs
@@ -3,21 +3,33 @@ using System.Diagnostics;
 
 namespace MisalignedSpace {
     class Misaligned {
-        static int printColorMap() {
-            string[] majorColors = {"White", "Red", "Black", "Yellow", "Violet"};
-            string[] minorColors = {"Blue", "Orange", "Green", "Brown", "Slate"};
-            int i = 0, j = 0;
-            for(i = 0; i < 5; i++) {
-                for(j = 0; j < 5; j++) {
+        static int GetTotalCount(string[] majorColors, string[] minorColors)
+        {
+            return majorColors.Length * minorColors.Length;
+        }
+
+        static void PrintColorMap(string[] majorColors, string[] minorColors)
+        {
+            for (int i = 0; i < majorColors.Length; i++)
+            {
+                for (int j = 0; j < minorColors.Length; j++)
+                {
                     Console.WriteLine("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[i]);
                 }
             }
-            return i * j;
         }
+
         static void Main(string[] args) {
-            int result = printColorMap();
-            Debug.Assert(result == 25);
+            string[] majorColors = { "White", "Red", "Black", "Yellow", "Violet" };
+            string[] minorColors = { "Blue", "Orange", "Green", "Brown", "Slate" };
+
+            PrintColorMap(majorColors, minorColors);
+
+            Debug.Assert(GetTotalCount(majorColors, minorColors) != 25);
+
             Console.WriteLine("All is well (maybe!)");
         }
+
+
     }
 }
