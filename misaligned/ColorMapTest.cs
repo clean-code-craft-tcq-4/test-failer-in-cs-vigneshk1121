d4be79ae503698113247474046887f5e509c41bc
APAC\EKG1COB <kennadi.vignesh@in.bosch.com>
2022-06-28 18:00:18 +0530

    updates for misaligned.cs

---
+++ misaligned/ColorMapTest.cs
@@ -0,0 +1,36 @@
+﻿using MisalignedSpace;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace misaligned
+{
+    public class ColorMapTest
+    {
+       List<ColorMap> ColorMap;
+
+       public ColorMapTest(List<ColorMap> colorMaps)
+        {
+            ColorMap = colorMaps;
+        }
+
+        public void TestColorMap(string[] majorColors, string[] minorColors)
+        {
+            for (int i = 0; i < ColorMap.Count; i++)
+            {
+                Debug.Assert(ColorMap[i] != null);
+                Debug.Assert(ColorMap[i].PairNumber == i+1);
+            }
+
+            Debug.Assert(GetTotalCount(majorColors, minorColors) == 25);
+        }
+
+        static int GetTotalCount(string[] majorColors, string[] minorColors)
+        {
+            return majorColors.Length * minorColors.Length;
+        }
+    }
+}
--- misaligned/misaligned.cs
+++ misaligned/misaligned.cs
@@ -1,35 +1,64 @@
-﻿using System;
+﻿using misaligned;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
-namespace MisalignedSpace {
-    class Misaligned {
-        static int GetTotalCount(string[] majorColors, string[] minorColors)
-        {
-            return majorColors.Length * minorColors.Length;
-        }
+namespace MisalignedSpace
+{
+    class Misaligned
+    {
 
-        static void PrintColorMap(string[] majorColors, string[] minorColors)
+
+        static List<ColorMap> GetColorMap(string[] majorColors, string[] minorColors)
         {
+            List<ColorMap> colorMaps = new List<ColorMap>();
             for (int i = 0; i < majorColors.Length; i++)
             {
                 for (int j = 0; j < minorColors.Length; j++)
                 {
-                    Console.WriteLine("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[i]);
+                    colorMaps.Add(new ColorMap
+                    {
+                        PairNumber = (i * 5 + j) + 1,
+                        MajorColor = majorColors[i],
+                        MinorColor = minorColors[j]
+                    });
                 }
+            }
+
+            return colorMaps;
+        }
+
+        static void PrintColorMap(List<ColorMap> colorMaps)
+        {
+            for (int i = 0; i < colorMaps.Count; i++)
+            {
+                Console.WriteLine("{0} | {1} | {2}", colorMaps[i].PairNumber, colorMaps[i].MajorColor, colorMaps[i].MinorColor);
+
             }
         }
 
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
             string[] majorColors = { "White", "Red", "Black", "Yellow", "Violet" };
             string[] minorColors = { "Blue", "Orange", "Green", "Brown", "Slate" };
 
-            PrintColorMap(majorColors, minorColors);
+           var colorMap = GetColorMap(majorColors, minorColors);
 
-            Debug.Assert(GetTotalCount(majorColors, minorColors) != 25);
+            PrintColorMap(colorMap);
+
+            ColorMapTest colorMapTest = new ColorMapTest(colorMap);
+            colorMapTest.TestColorMap(majorColors, minorColors);
 
             Console.WriteLine("All is well (maybe!)");
         }
+    }
 
+   public class ColorMap
+    {
+        public int PairNumber { get; set; }
 
+        public string? MajorColor { get; set; }
+
+        public string? MinorColor { get; set; }
     }
 }
